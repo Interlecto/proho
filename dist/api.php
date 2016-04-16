f23e2e0253070a4218ca40d6972d112ec70d5cc6
chlewey <chlewey@gmail.com>
2016-04-16 18:25:56 -0500

    entry points

---
+++ dist/api.php
@@ -0,0 +1,30 @@
+<?php
+/* api.php
+ * @author: Carlos Thompson
+ * 
+ * This is the entry point for the HTTP API. Requests to this API
+ * are forwarded to the respective Engine (module), and after checking
+ * for propper credentials, the response is provided, usually as a
+ * JSON object.
+ */
+
+// Development mode, shall be removed for production 
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+// API configuration
+if(file_exists("config/api.php"))
+	require_once "config/api.php";
+
+$response = include 'lib/api.php';
+if(empty($response)) {
+	header("HTTP/1.1 204 No Content");
+} else {
+	if(preg_match('/(?i)msie [5-8]/',$_SERVER['HTTP_USER_AGENT']))
+		header('ContentType: text/javascript');
+	else
+		header('ContentType: application/json');
+	echo json_encode($response);
+}
+
+?>
---
+++ dist/index.php
@@ -0,0 +1,26 @@
+<?php
+/* index.php
+ * @author: Carlos Thompson
+ * 
+ * This is the entry point for the Web App. Requests to this App
+ * are forwarded to the respective Engine (module), and after checking
+ * for propper credentials, the response is provided, as a formated
+ * HTML page using a skin and a template.
+ */
+
+// Development mode, shall be removed for production 
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+// API configuration
+if(file_exists("config/api.php"))
+	require_once "config/api.php";
+
+// check if content is catched
+require "lib/catches.php";
+
+// generate content
+$page = require "lib/page.php";
+$page->go();
+$page->close();
+?>
